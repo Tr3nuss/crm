634b76d2ff5db2000a62028baf032af42a010bed
Ivan Pronin <pacernestit@rambler.ru>
2024-07-12 21:31:04 +0300

    dev#auth feat (добавил SimpleLockScreen)

---
+++ src/components/authorization/simple/SimpleLockScreen.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import Avatar from "components/common/Avatar";
+import LockScreenForm from "../LookScreenForm";
+import team1 from "assets/img/team/1.jpg";
+
+const SimpleLockScreen: FC = () => {
+  return (
+    <div className="text-center">
+      <Avatar src={team1} size="4xl" />
+      <h5 className="mt-3 mb-0">Hi! Emma Watson</h5>
+      <small>Enter your password to access the admin.</small>
+      <LockScreenForm className="mt-4 mx-sm-4" type="simple" />
+    </div>
+  );
+};
+
+export default SimpleLockScreen;
