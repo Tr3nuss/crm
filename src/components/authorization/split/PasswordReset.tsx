a22427afd955e7842bffd3c5e073e2fd527223b4
Ivan Pronin <pacernestit@rambler.ru>
2024-07-12 15:57:49 +0300

    dev#auth feat (добавил PasswordReset)

---
+++ src/components/authorization/split/PasswordReset.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import PasswordResetForm from "components/authentication/PasswordResetForm";
+import bgImg from "assets/img/generic/20.jpg";
+import AuthSplitLayout from "layouts/AuthSplitLayout";
+
+export const PasswordReset: FC = () => {
+  return (
+    <AuthSplitLayout bgProps={{ image: bgImg }}>
+      <div className="text-center">
+        <h3>Reset password</h3>
+      </div>
+      <PasswordResetForm layout="split" hasLabel />
+    </AuthSplitLayout>
+  );
+};
+
+export default PasswordReset;
