1256de2e47434e64662090baa845ed08464086c3
Ivan Pronin <pacernestit@rambler.ru>
2024-07-12 21:24:50 +0300

    dev#auth feat (добавил SimpleLogout)

---
+++ src/components/authorization/simple/SimpleLogout.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import LogoutContent from "../LogoutContent";
+
+const SimpleLogout: FC = () => (
+  <div className="text-center">
+    <LogoutContent />
+  </div>
+);
+
+export default SimpleLogout;
