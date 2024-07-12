c7786854096c2775b50634b448f0681e5c6848c5
Ivan Pronin <pacernestit@rambler.ru>
2024-07-12 20:56:30 +0300

    dev#auth feat (добавил ConfirmMail)

---
+++ src/components/authorization/split/ConfirmMail.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+import ConfirmMailContent from "../ConfirmMailContent";
+import bgImg from "assets/img/generic/16.jpg";
+import AuthSplitLayout from "layouts/AuthSplitLayout";
+
+export const ConfirmMail: FC = () => {
+  return (
+    <AuthSplitLayout bgProps={{ image: bgImg, position: "50% 30%" }}>
+      <div className="text-center">
+        <ConfirmMailContent email="xyz@abc.com" layout="split" titleTag="h3" />
+      </div>
+    </AuthSplitLayout>
+  );
+};
+
+export default ConfirmMail;
