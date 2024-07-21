616d8d89aab824106b8e1c6e4b836239fed2d3fe
Ivan Pronin <pacernestit@rambler.ru>
2024-07-21 17:08:42 +0300

    dev#auth feat (привязал роуты к компонентам в auth)

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,8 +1,23 @@
 import { FC } from "react";
 import { Route, Routes } from "react-router-dom";
+import SimpleRegistration from "../components/authorization/simple/SimpleRegistration";
+import routePath from "./routePath";
+import SimpleLogin from "../components/authorization/simple/SimpleLogin";
+import Login from "../components/authorization/split/Login";
+import Registration from "../components/authorization/split/Registration";
+import CardLogin from "../components/authorization/card/CardLogin";
+import CardRegistration from "../components/authorization/card/CardRegistration";
 
-export const Routes: FC = () => {
-    return (
-        <Routes
-    )
-}
+export const RouteNav: FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<SimpleRegistration />} />
+      <Route path={routePath.simpleLogin} element={<SimpleLogin />} />
+      <Route path={routePath.simpleRegister} element={<SimpleRegistration />} />
+      <Route path={routePath.splitLogin} element={<Login />} />
+      <Route path={routePath.splitRegister} element={<Registration />} />
+      <Route path={routePath.cardLogin} element={<CardLogin />} />
+      <Route path={routePath.cardRegister} element={<CardRegistration />} />
+    </Routes>
+  );
+};
