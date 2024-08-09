7dba2b924b75fe7b0dfb6af13139c78c17f015d0
Ivan Pronin <pacernestit@rambler.ru>
2024-08-09 21:59:16 +0300

    dev fix (удалил неиспользуемые компоненты)

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -1,13 +1,13 @@
-export const rootPaths = {
-  root: "/",
-  authRoot: "authentication",
-  authSimpleRoot: "simple",
-  authCardRoot: "card",
-  authSplitRoot: "split",
-};
+// export const rootPaths = {
+//   root: "/",
+//   authRoot: "authentication",
+//   authSimpleRoot: "simple",
+//   authCardRoot: "card",
+//   authSplitRoot: "split",
+// };
 
-export default {
-  simpleLogin: `/${rootPaths.authRoot}/${rootPaths.authSimpleRoot}/login`,
-  cardLogin: `/${rootPaths.authRoot}/${rootPaths.authCardRoot}/login`,
-  splitLogin: `/${rootPaths.authRoot}/${rootPaths.authSplitRoot}/login`,
-};
+// export default {
+//   simpleLogin: `/${rootPaths.authRoot}/${rootPaths.authSimpleRoot}/login`,
+//   cardLogin: `/${rootPaths.authRoot}/${rootPaths.authCardRoot}/login`,
+//   splitLogin: `/${rootPaths.authRoot}/${rootPaths.authSplitRoot}/login`,
+// };
