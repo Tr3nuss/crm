96d2ebba8fd5952ba466ff18f8e8807064612234
Ivan Pronin <pacernestit@rambler.ru>
2024-08-14 17:02:08 +0300

    dev feat (внес правки в типы в HomePage)

--- src/components/main/mainPage.tsx
+++ src/components/main/mainPage.tsx
@@ -2,47 +2,48 @@ import { FC, useState, useEffect } from "react";
 import axios from "axios";
 
 export const MainPage: FC = () => {
-    const [userData, setUserData] = useState(null);
-    const [error, setError] = useState(null);
+  const [userData, setUserData] = useState<object | null>(null);
+  const [error, setError] = useState<string | Error | null>(null);
 
-    useEffect(() => {
-        // Функция для извлечения токена из URL
-        const getTokenFromUrl = () => {
-            const params = new URLSearchParams(window.location.hash.slice(1));
-            return params.get('access_token');
-        };
+  useEffect(() => {
+    const getTokenFromUrl = () => {
+      const params = new URLSearchParams(window.location.hash.slice(1));
+      return params.get("access_token");
+    };
 
-        const fetchUserData = async (token: string) => {
-            try {
-                const response = await axios.get("https://387f47aeacc8.vps.myjino.ru/api", {
-                    headers: {
-                        Authorization: token
-                    }
-                });
-                setUserData(response.data);
-            } catch (err: any | string) {
-                setError(err);
-                console.error('Ошибка при получении данных о пользователе:', err);
-            }
-        };
+    const fetchUserData = async (token: string) => {
+      try {
+        const response = await axios.get(
+          "https://387f47aeacc8.vps.myjino.ru/api",
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        );
+        setUserData(response.data);
+      } catch (err: any | string) {
+        setError(err);
+        console.error("Ошибка при получении данных о пользователе:", err);
+      }
+    };
 
-        const token = getTokenFromUrl();
-        if (token) {
-            fetchUserData(token);
-        }
-    }, []);
+    const token = getTokenFromUrl();
+    if (token) {
+      fetchUserData(token);
+    }
+  }, []);
 
-    return (
+  return (
+    <div>
+      {error && <p>Ошибка</p>}
+      {userData ? (
         <div>
-            {error && <p>Ошибка</p>}
-            {userData ? (
-                <div>
-                    <h1>Данные пользователя</h1>
-                    <pre>{JSON.stringify(userData, null, 2)}</pre>
-                </div>
-            ) : (
-                <p>Загрузка данных...</p>
-            )}
+          <pre>{JSON.stringify(userData, null, 2)}</pre>
         </div>
-    );
+      ) : (
+        <p>Загрузка данных...</p>
+      )}
+    </div>
+  );
 };
