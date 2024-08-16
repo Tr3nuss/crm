cf26deb2da44d7a2199d062b7dd38b94ad5a1aac
Ivan Pronin <pacernestit@rambler.ru>
2024-08-16 23:16:24 +0300

    4

--- src/components/authorization/simple/SimpleLogin.tsx
+++ src/components/authorization/simple/SimpleLogin.tsx
@@ -43,6 +43,7 @@ export const SimpleLogin: FC = () => {
       console.log("Сообщение с токеном", data);
       //@ts-ignore
       localStorage.setItem("O-auth-token", data);
+      window.location.href = "/main_page";
     })
     .catch((error: AuthError) => console.log("Обработка ошибки", error));
 
