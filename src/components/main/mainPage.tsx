ac5afbe554d96714eeb62c1aea74e8d21a3d6762
Ivan Pronin <pacernestit@rambler.ru>
2024-08-17 23:53:26 +0300

    почистил мусор, победа бтв №2

--- src/components/main/mainPage.tsx
+++ src/components/main/mainPage.tsx
@@ -8,10 +8,9 @@ export const MainPage: FC = () => {
     last_name: string;
     email: string;
     phone: string;
-  }
+  } // интерфейс для отрисовки данных юзера
 
   const [userData, setUserData] = useState<UserData | null>(null);
-  const [error, setError] = useState<string | Error | null>(null);
 
   useEffect(() => {
     //@ts-ignore
@@ -20,17 +19,13 @@ export const MainPage: FC = () => {
     });
 
     const fetchUserData = async () => {
-      //@ts-ignore
-
       let token = localStorage.getItem("O-auth-token");
 
       window.onload = () => {
         if (!token) {
-          window.location.href = "/simple_login";
+          window.location.href = "/simple_login"; // редирект в случае незареганного пользователя
         }
       };
-
-      console.log(token);
 
       try {
         const response = await axios.get<UserData>(
@@ -39,12 +34,9 @@ export const MainPage: FC = () => {
             params: { authToken: token },
           }
         );
-        console.log(response.data);
         setUserData(response.data);
       } catch (err: any | string) {
-        setError(err);
         window.location.href = "/simple_login";
-        console.error("Ошибка при получении данных о пользователе:", err);
       }
     };
 
@@ -53,9 +45,16 @@ export const MainPage: FC = () => {
 
   return (
     <div>
-      {error && <p>Ошибка: {error.toString()}</p>}
       {userData ? (
-        <p>
+        <p
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+            fontSize: 18,
+            fontWeight: 500,
+          }}
+        >
           {userData.login}, {userData.first_name}, {userData.last_name}{" "}
           {userData.email}, {userData.phone}
         </p>
