f6826920f61c6e7ae39260e085670de5467709aa
Ivan Pronin <pacernestit@rambler.ru>
2024-10-01 19:17:43 +0300

    dev fix

--- src/components/admin/adminPage.tsx
+++ src/components/admin/adminPage.tsx
@@ -228,30 +228,26 @@ const AdminPage: FC = () => {
               justifyContent: "space-around",
             }}
           >
-            <Box>
-              <div>{item.name}</div>
-              <div>{item.translit}</div>
-              <div>{item.type}</div>
-              <div>{item.description}</div>
-              <Button
-                sx={{
-                  width: "300px",
-                  bgcolor: "#0871A4",
-                  textTransform: "none",
-                  color: "#fff",
-                  "&:hover": { bgcolor: "#0871A4" },
-                }}
-                //@ts-ignore
-                onClick={() => handleChangeDataUpdateForm(item.id)}
-              >
-                Редактировать
-              </Button>
-            </Box>
+            <div>{item.name}</div>
+            <div>{item.translit}</div>
+            <div>{item.type}</div>
+            <div>{item.description}</div>
+            <Button
+              sx={{
+                width: "300px",
+                bgcolor: "#0871A4",
+                textTransform: "none",
+                color: "#fff",
+                "&:hover": { bgcolor: "#0871A4" },
+              }}
+              //@ts-ignore
+              onClick={() => handleChangeDataUpdateForm(item.id)}
+            >
+              Редактировать
+            </Button>
 
             {editId === item.id && (
-              <Box
-                sx={{ display: "flex", flexDirection: "column", gap: "10px" }}
-              >
+              <Box sx={{ display: "flex", gap: "10px" }}>
                 <TextField
                   label="Имя"
                   size="small"
