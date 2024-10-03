d461426c69755362ad02158a510d5cc94ea2a6e2
Ivan Pronin <pacernestit@rambler.ru>
2024-10-03 16:26:27 +0300

    dev fix

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,7 +2,6 @@ import { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 import SimpleLogin from "../components/authorization/simple/SimpleLogin";
 import { MainPage } from "../components/main/mainPage";
-import AdminPage from "../components/admin/adminPage";
 
 export const RouteNavigation: FC = () => {
   return (
@@ -10,7 +9,6 @@ export const RouteNavigation: FC = () => {
       <Route path="/" element={<SimpleLogin />} />
       <Route path="/simple_login" element={<SimpleLogin />} />
       <Route path="/main_page" element={<MainPage />} />
-      <Route path="/admin_page" element={<AdminPage />} />
     </Routes>
   );
 };
